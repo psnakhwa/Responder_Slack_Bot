f18efc22aafd4fcbd6c339fcef61fb320fb8c1b0
dupandit <dupandit@ncsu.edu>
2017-10-25 20:08:07 -0400

    Adding sample helper js file and one mock json file

--- helper.js
+++ helper.js
@@ -1,69 +1,190 @@
 // for finding list of people who can work on current repo
-var repoName =  "TraziBot-Testing";
 var token = "token " + process.env.GITHUB_TOKEN;
 var urlRoot = "https://github.ncsu.edu/api/v3";
-var userId = "sushantgupta1206";
+var data = require('./mock.json');
+var nock = require("nock");
+var _ = require("underscore");
+var request = require("request");
+var querystring = require('querystring');
+var Promise = require("bluebird");
+
+//console.log(data);
+var mockIssueList = nock("https://github.ncsu.edu/api/v3")
+.persist() // This will persist mock interception for lifetime of program.
+.get("/repos/dupandit/Sample-mock-repo/issues")
+.reply(200, JSON.stringify(data.issueList) );
+
+var mockCollaborators = nock("https://github.ncsu.edu/api/v3")
+.persist() // This will persist mock interception for lifetime of program.
+.get("/repos/dupandit/Sample-mock-repo/collaborators")
+.reply(200, JSON.stringify(data.collaborators) );
+
+var mockCommits = nock("https://github.ncsu.edu/api/v3")
+.persist() // This will persist mock interception for lifetime of program.
+.get("/repos/dupandit/Sample-mock-repo/commits")
+.reply(200, JSON.stringify(data.commits_of_a_file) );
+
+var mockReviewers = nock("https://github.ncsu.edu/api/v3")
+.persist() // This will persist mock interception for lifetime of program.
+.get("/repos/dupandit/Sample-mock-repo/reviewers")
+.reply(200, JSON.stringify(data.reviewers) );
+
+var issuedetails = nock("https://github.ncsu.edu/api/v3")
+.persist() // This will persist mock interception for lifetime of program.
+.get("/repos/dupandit/Sample-mock-repo/issues/1")
+.reply(200, JSON.stringify(data.issueList[0]) );
+
+
 
 /**
- * @desc  
- * @param 
- * @return 
+ * @desc
+ * @param
+ * @return
  */
+function getCollaborators(owner,repo){
 
-// manager passes his git token (repo) : bot returns all the repos manager is a part of 
-function getAllrepos(){
-    //get all the repos where manager is a collaborator
-    // pass all the repos from this function into the get collaborators function
-}
-//using these repos get a list of all employees who have contributed to the projects
-function callGetCollaborator(){
-    //var repoName =  "TraziBot-Testing";
-   //var token = "token " + process.env.GITHUB_TOKEN;
-    //var urlRoot = "https://github.ncsu.edu/api/v3";
-    //var userId = "sushantgupta1206";
-        
-   getCollaborators(userId,repoName);    
-}
-
-function getCollaborators(userName, repoName){
-
-    var options = { //dictionary
-       url: urlRoot + '/repos/' + userName + "/" + reposName + "/collaborators",
-       ///repos/:owner/:repo/collaborators    
+    var options = {
+        url: urlRoot + "/repos/" + owner + "/" + repo + "/collaborators",
         method: 'GET',
-                headers: {
+        headers: {
                      "User-Agent": "EnableIssues",
                      "content-type": "application/json",
                      "Authorization": token
                  }
-            };
-        
+        };
+
+        return new Promise(function (resolve, reject)
+        {
              // Send a http request to url and specify a callback that will be called upon its return.
              request(options, function (error, response, body) {
                  var obj = JSON.parse(body);
-                 //console.log( obj );
-                 for (var i = 0; i < obj.length; i++) {
-                     var name = obj[i].name;
-                     console.log(name);
-                 }
+                 resolve(obj);
              });
-        
-
-    //send git repo name to get all colloborators
-    console.log("getCollaborators start");
-    //bot.reply("this is the list of colloboprators");
-    console.log("getCollaborators end");
+        });
 }
 
 /**
- * @desc  
- * @param 
- * @return 
+ * @desc this will assign userId to issueNumber
+ * @param userId emp to whom we will assign an issue
+ * @param issueNumber issue to assigned
+ * @return
  */
-function assignIssueToEmp(){
-    console.log("assignIssueToEmp start");
-    console.log("assignIssueToEmp end");
-} 
+function assignIssueToEmp(userId, issueNumber){
 
+
+    return new Promise(function(resolve, reject){
+        resolve("Issue " + issueNumber + " assigned to " + userId);
+    });
+    console.log("assignIssueToEmp end");
+}
+
+
+/**
+ * @desc
+ * @param
+ * @return
+ */
+function assignReviewerForIssue(userId, issueNumber){
+    console.log("assignReviewerForIssue start");
+    return new Promise(function(resolve, reject){
+        resolve("Reviewer " + userId + " assigned to issue #" + issueNumber);
+    });
+    console.log("assignReviewerForIssue end");
+}
+
+function getPossibleAssignees(issueNumber){
+    var assignees = data.users;
+    return assignees;
+}
+
+function isValidUser(userId, userList){
+
+    return new Promise(function (resolve, reject)
+	{
+         if(userList.indexOf(userId)>-1){
+            resolve(userId);
+        }
+        reject(userId);
+    });
+}
+
+// Usecase 1 :
+
+
+
+// Usecase 2 :
+function Listofcommits(owner,repo) {
+  var options = {
+      url: urlRoot + "/repos/" + owner + "/" + repo + "/commits",
+      method: 'GET',
+      headers: {
+                   "User-Agent": "EnableIssues",
+                   "content-type": "application/json",
+                   "Authorization": token
+               }
+      };
+
+      return new Promise(function (resolve, reject)
+      {
+           // Send a http request to url and specify a callback that will be called upon its return.
+           request(options, function (error, response, body) {
+               var obj = JSON.parse(body);
+               resolve(obj);
+           });
+      });
+}
+
+
+// Usecase 3 :
+function Listofissues(owner,repo) {
+  var options = {
+      url: urlRoot + "/repos/" + owner + "/" + repo + "/issues",
+      method: 'GET',
+      headers: {
+                   "User-Agent": "EnableIssues",
+                   "content-type": "application/json",
+                   "Authorization": token
+               }
+      };
+
+      return new Promise(function (resolve, reject)
+      {
+           // Send a http request to url and specify a callback that will be called upon its return.
+           request(options, function (error, response, body) {
+               var obj = JSON.parse(body);
+               resolve(obj);
+           });
+      });
+}
+
+
+function getIssueDetails(owner,repo,number){
+
+    var options = {
+        url: urlRoot + "/repos/" + owner + "/" + repo + "/issues/"+number,
+        method: 'GET',
+        headers: {
+                     "User-Agent": "EnableIssues",
+                     "content-type": "application/json",
+                     "Authorization": token
+                 }
+        };
+
+    return new Promise(function (resolve, reject)
+    {
+            // Send a http request to url and specify a callback that will be called upon its return.
+            request(options, function (error, response, body) {
+                var obj = JSON.parse(body);
+                resolve(obj);
+            });
+    });
+}
+
+exports.getIssueDetails = getIssueDetails
+exports.Listofcommits = Listofcommits
 exports.getCollaborators = getCollaborators;
+exports.Listofissues = Listofissues;
 exports.assignIssueToEmp = assignIssueToEmp;
+exports.isValidUser = isValidUser;
+exports.getPossibleAssignees = getPossibleAssignees;
+exports.assignReviewerForIssue = assignReviewerForIssue;
