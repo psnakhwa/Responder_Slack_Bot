740888a0a0e91314e3f89e97663bdd5bbb8ff3c1
sbshete <sbshete@ncsu.edu>
2017-11-07 18:23:52 -0500

    added few queries

--- js/mysql.js
+++ js/mysql.js
@@ -1,4 +1,4 @@
- var mysql      = require('mysql');
+var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -13,13 +13,13 @@ function getConnection(){
 
 function getIssueTags(issueNumber){
     //connection.connect();
-    var query = 'select Issue_tags from issue_table where Issue_ID=' + issueNumber;//+ ';';
+    var query = 'select tags from issue_tags where Issue_ID=' + issueNumber;//+ ';';
     connection.query(query, function(err, rows, fields) {
         if (!err){
             var data = rows;
             console.log(data);
             for(var i=0;i<data.length;i++){
-                console.log(data[i].Issue_tags.split(','));
+                console.log(data[i].Issue_tags);
             }
         }
         else
@@ -28,9 +28,12 @@ function getIssueTags(issueNumber){
     //connection.end();
 }
 
-function getReviewerTags(userId){
+function getUserTags(userIds){
     //connection.connect();
     console.log('here 1');
+    userIds.forEach(function(user){
+        
+    });
     var query = 'select User_tags from user_table where User_ID=\'' + userId + '\'';
     connection.query(query, function(err, rows, fields) {
         if (!err){
@@ -83,7 +86,7 @@ function updateUserTags(data){
     });
 }
 
-exports.getReviewerTags = getReviewerTags;
+exports.getUserTags = getUserTags;
 exports.getIssueTags = getIssueTags;
 exports.getConnection = getConnection;
 exports.insertIssueTags = insertIssueTags;
