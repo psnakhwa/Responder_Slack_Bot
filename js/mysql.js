31d8c2238d5eb79eb74ed0694f1aabcf85c730ed
asagarwa <asagarwa@ncsu.edu>
2017-11-11 16:25:51 -0500

    Update mysql.js

--- js/mysql.js
+++ js/mysql.js
@@ -28,7 +28,7 @@ function getIssueTags(issueNumber){
     //connection.end();
 }
 
-function getUserTagsCount(userList, tagsList){
+function getUserTagsCount(userList, tagsList,assignee){
     return new Promise(function(resolve, reject){
         var data = {};
         console.log("get tag count");
@@ -36,7 +36,9 @@ function getUserTagsCount(userList, tagsList){
         var userProcessed = 0;
         for(var i=0;i<userList.length;i++){
             UserTagsCount(userList[i], tagsList).then(function(userCount){
-                data[userCount.split(":")[0]] = userCount.split(":")[1];
+                if((userCount.split(":")[1] != 0) && (userCount.split(":")[0] != assignee)){
+                    data[userCount.split(":")[0]] = userCount.split(":")[1];
+                }
                 console.log("pushing data");
                 console.log("user: ", userCount.split(":")[0]);
                 console.log(data);
@@ -53,28 +55,16 @@ function getUserTagsCount(userList, tagsList){
 }
 
 function UserTagsCount(user, tagsList){
-    //connection.connect();
-    //tagsList = ['c++','java'];
-    //userList = ['sbshete','sagupta'];
     return new Promise(function(resolve, reject){
-        //var query = "select User_ID, count(skill) as skillCount from user_tags where User_ID in ('" + userList.join("','") + "')" +
-        //            "and skill in ('" + tagsList.join("','") + "') group by User_ID order by count(skill) desc limit 3";
         var query = "select count(distinct a.User_ID, b.Issue_ID) as numOfIssues from user_tags as a,"+
                     "(select Issue_ID, tags from issue_tags where Issue_id in "+
                     "(select Issue_ID from issue_assignee where User_Id='"+ user + "')) as b where a.skill=b.tags"+
                     " and User_Id='" + user + "' and b.tags in('" + tagsList.join("','") + "');";
         console.log(query);
-        var assigneeList = [];
         connection.query(query, function(err, rows, fields) {
             if (!err){
-                //var data = rows;
                 console.log('in here');
                 console.log(rows);
-                // for(var i=0;i<rows.length;i++){
-                //     console.log(rows[i].User_ID+ " " + rows[i].skillCount);
-                //     assigneeList.push(rows[i].User_ID);
-                // }
-                // resolve(assigneeList);
                 resolve(user+":"+rows[0].numOfIssues);
             }
             else{
@@ -83,7 +73,53 @@ function UserTagsCount(user, tagsList){
             }   
         });
     });
-    //connection.end();
+}
+
+function getReviewerTagsCount(userList, tagsList,assignee){
+    return new Promise(function(resolve, reject){
+        var data = {};
+        console.log("get tag count");
+        var i=0;
+        var userProcessed = 0;
+        for(var i=0;i<userList.length;i++){
+            ReviewerTagsCount(userList[i], tagsList).then(function(userCount){
+                if((userCount.split(":")[1] != 0) && (userCount.split(":")[0] != assignee)){
+                    data[userCount.split(":")[0]] = userCount.split(":")[1];
+                }
+                console.log("pushing data");
+                console.log("user: ", userCount.split(":")[0]);
+                console.log(data);
+                userProcessed++;
+                if(userProcessed == userList.length){
+                    console.log("user: ", userProcessed);
+                    resolve(data);
+                }
+            });
+        }
+        console.log("data: ", data);
+        //resolve(data);
+    });
+}
+
+function ReviewerTagsCount(user, tagsList){
+    return new Promise(function(resolve, reject){
+        var query = "select count(distinct a.User_ID, b.Issue_ID) as numOfIssues from user_tags as a,"+
+                    "(select Issue_ID, tags from issue_tags where Issue_id in "+
+                    "(select Issue_ID from issue_reviewer where User_Id='"+ user + "')) as b where a.skill=b.tags"+
+                    " and User_Id='" + user + "' and b.tags in('" + tagsList.join("','") + "');";
+        console.log(query);
+        connection.query(query, function(err, rows, fields) {
+            if (!err){
+                console.log('in here');
+                console.log(rows);
+                resolve(user+":"+rows[0].numOfIssues);
+            }
+            else{
+                console.log(err);
+                reject("Error while fetching user-tags details");
+            }   
+        });
+    });
 }
 
 function insertIssueReviewers(users,issueNumber){
@@ -143,6 +179,7 @@ function updateUserTags(data){
     });
 }
 
+exports.getReviewerTagsCount = getReviewerTagsCount;
 exports.insertIssueReviewers = insertIssueReviewers;
 exports.getUserTagsCount = getUserTagsCount;
 exports.getIssueTags = getIssueTags;
